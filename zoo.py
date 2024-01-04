68de82bc30ca7bbf0c9f69d844bba20ec5b5da4d
shecanfixme <gorocsmartonxd@gmail.com>
2024-01-04 13:24:11 +0100

    Állat hozzáadása

--- zoo.py
+++ zoo.py
@@ -1,20 +1,24 @@
-zoo = []
+def addAnimal(myName):
+    animal = dict()
+    found = False
+    for a in zoo:
+        if myName in a.keys():
+                a[myName] += 1
+                found = True
+                #break
+    if not found:
+        animal[myName] = 1
+        zoo.append(animal)
+
+zoo = [{"Kecske": 1}]
 
 print("Ez egy állatkert.")
 print("Állat hozzáadása (1) - elvétele (2) - kilépés (0)")
 select = None
 while select != "0":
     select = input("Mit szeretne tenni? ")
-    if select != 0:
+    if select != "0":
         if select == "1":
-            #animal = dict()
             name = input("A dög neve: ")
-            if name not in animal.keys():
-                animal[name] = 1
-                zoo.append(animal)
-            else:
-                for a in zoo:
-                    a[name] += 1   
-            animal = dict()
-for a in zoo:
-    print(a)
+            addAnimal(name) 
+print(f"Második: {zoo}")
